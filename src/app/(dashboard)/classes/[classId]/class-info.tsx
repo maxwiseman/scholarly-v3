995708c04d301a408367f22150509a86cb4b1889
maxwiseman <13maxwellwise@gmail.com>
2023-12-22 19:42:07 -0500

    fix: fixed some inconsistent styles

--- src/app/(dashboard)/classes/[classId]/class-info.tsx
+++ src/app/(dashboard)/classes/[classId]/class-info.tsx
@@ -16,7 +16,7 @@ export function ClassInfo({ classId }: { classId: string }): React.ReactNode {
 
   if (!classFetcher.isFetched)
     return (
-      <div className="flex h-[30vh] min-h-[12rem] flex-col justify-center bg-card p-8">
+      <div className="flex h-[30vh] min-h-[12rem] flex-col justify-center bg-card p-8 shadow-[inset_0px_-3px_44px_-26px_rgba(0,0,0,0.4)] dark:shadow-none">
         <Skeleton className="h-9 w-96" />
         <div className="mt-2 flex flex-row gap-2">
           <Skeleton className="h-[22px] w-28" />
@@ -25,10 +25,7 @@ export function ClassInfo({ classId }: { classId: string }): React.ReactNode {
     );
 
   return (
-    <div
-      style={{ boxShadow: "inset 0px -3px 44px -26px rgba(0,0,0,0.4)" }}
-      className="flex h-[30vh] min-h-[12rem] flex-col justify-center bg-card p-8"
-    >
+    <div className="flex h-[30vh] min-h-[12rem] flex-col justify-center bg-card p-8 shadow-[inset_0px_-3px_44px_-26px_rgba(0,0,0,0.4)] dark:shadow-none">
       <h1 className="mt-0 text-3xl font-bold">{classInfo?.name}</h1>
       {/* <h4 className="text-muted-foreground">Lorem ipsum dolor sit amet</h4> */}
       <div className="mt-2 flex flex-row gap-2">
