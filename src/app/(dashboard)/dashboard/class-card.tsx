86e84db115cea6b5bc61964004885374a5c1b1e9
maxwiseman <13maxwellwise@gmail.com>
2023-12-24 12:56:24 -0600

    feat: added edit class menu

--- src/app/(dashboard)/dashboard/class-card.tsx
+++ src/app/(dashboard)/dashboard/class-card.tsx
@@ -1,10 +1,28 @@
+import { Button } from "@/app/_components/ui/button";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@/app/_components/ui/card";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/app/_components/ui/context-menu";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { Input } from "@/app/_components/ui/input";
 import { Separator } from "@/app/_components/ui/separator";
+import { IconPencil } from "@tabler/icons-react";
 import Link from "next/link";
 
 export function ClassCard({
@@ -24,28 +42,53 @@ export function ClassCard({
   };
 }) {
   return (
-    <Link
-      className="rounded-xl focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
-      href={`/classes/${classData.id}`}
-    >
-      <Card className="shadow-sm">
-        <CardHeader className="flex flex-row items-center gap-2 space-y-0 py-3 pb-0">
-          {/* <Avatar className="h-7 w-7">
-            <AvatarFallback>
-              <IconUser />
-            </AvatarFallback>
-          </Avatar> */}
-          <CardTitle className="mt-0 line-clamp-1 text-xl font-semibold">
-            {classData.name}
-          </CardTitle>
-        </CardHeader>
-        <Separator className="my-2" />
-        <CardContent className="pb-3 text-muted-foreground">
-          Content here <br />
-          Content here <br />
-          Content here <br />
-        </CardContent>
-      </Card>
-    </Link>
+    <Dialog>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <Link
+            className="rounded-xl focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+            href={`/classes/${classData.id}`}
+          >
+            <Card className="shadow-sm">
+              <CardHeader className="flex flex-row items-center gap-2 space-y-0 py-3 pb-0">
+                {/* <Avatar className="h-7 w-7">
+                <AvatarFallback>
+                  <IconUser />
+                </AvatarFallback>
+              </Avatar> */}
+                <CardTitle className="mt-0 line-clamp-1 text-xl font-semibold">
+                  {classData.name}
+                </CardTitle>
+              </CardHeader>
+              <Separator className="my-2" />
+              <CardContent className="pb-3 text-muted-foreground">
+                Content here <br />
+                Content here <br />
+                Content here <br />
+              </CardContent>
+            </Card>
+          </Link>
+        </ContextMenuTrigger>
+        <ContextMenuContent>
+          <DialogTrigger asChild>
+            <ContextMenuItem>
+              <IconPencil className="mr-2 h-4 w-4" /> Edit class
+            </ContextMenuItem>
+          </DialogTrigger>
+        </ContextMenuContent>
+      </ContextMenu>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit class</DialogTitle>
+          <DialogDescription>
+            Make changes to the class here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <Input />
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/app/(dashboard)/navbar.tsx
+++ src/app/(dashboard)/navbar.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "../_components/ui/user-button";
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 flex h-14 items-center justify-between border-b border-border bg-background/95 px-8 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/75">
+    <nav className="sticky top-0 z-40 flex h-14 items-center justify-between border-b border-border bg-background/95 px-8 shadow-sm backdrop-blur supports-[backdrop-filter]:bg-background/75">
       <div className="flex flex-row items-center justify-start">
         <Link href={"/dashboard"}>
           <span className="block font-bold">Scholarly</span>
