98e572799702a16eb8c8e25d0f6c9b18641e1675
maxwiseman <13maxwellwise@gmail.com>
2023-12-30 13:40:49 -0500

    feat: added basic course overview

--- src/app/(dashboard)/classes/[classId]/page.tsx
+++ src/app/(dashboard)/classes/[classId]/page.tsx
@@ -1,3 +1,53 @@
-export default function Page(): React.ReactNode {
-  return <>This is the class overview</>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/_components/ui/table";
+import { api } from "@/trpc/server";
+
+export default async function Page({
+  params,
+}: {
+  params: { classId: string };
+}): Promise<React.ReactElement> {
+  const categories = await api.aspen.getCategories.query({
+    id: params.classId,
+  });
+
+  return (
+    <main>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Category</TableHead>
+            <TableHead>Weight</TableHead>
+            <TableHead>Average</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {categories.categories.map((category) => {
+            return (
+              <TableRow key={category.name}>
+                <TableCell>{category.name}</TableCell>
+                <TableCell>{category.weight * 100}%</TableCell>
+                <TableCell>
+                  {isNaN(category.value) ? "" : `${category.value}%`}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={2}>Gradebook Average</TableCell>
+            <TableCell>{categories.average}%</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </main>
+  );
 }
