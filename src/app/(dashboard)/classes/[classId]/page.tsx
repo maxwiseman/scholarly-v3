c4d501c368902b38e0df5f0187d889fb1b8d399c
maxwiseman <13maxwellwise@gmail.com>
2023-12-31 12:51:42 -0500

    feat: added a todo list

--- src/app/(dashboard)/classes/[classId]/page.tsx
+++ src/app/(dashboard)/classes/[classId]/page.tsx
@@ -22,15 +22,32 @@ export default function Page({
   }
 
   return (
-    <main>
+    <main className="grid gap-8 lg:grid-cols-2">
       <Card>
-        <CardHeader>
-          <CardTitle>Missing Assignments</CardTitle>
+        <CardHeader className="pb-4">
+          <CardTitle className="text-lg font-bold">
+            Missing Assignments
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <ul>
             {assignmentData.data?.map((assignment) => {
               if (assignment.score === "M" || assignment.score === 0) {
+                return <li key={assignment.name}>{assignment.name}</li>;
+              }
+              return null;
+            })}
+          </ul>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="pb-4">
+          <CardTitle className="text-lg font-bold">Todo List</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            {assignmentData.data?.map((assignment) => {
+              if (new Date(assignment.dateDue) > new Date()) {
                 return <li key={assignment.name}>{assignment.name}</li>;
               }
               return null;
