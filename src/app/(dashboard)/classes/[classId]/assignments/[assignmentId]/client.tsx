c25a677067d9edaf759a232818f8a2fe3f79db76
maxwiseman <13maxwellwise@gmail.com>
2024-01-12 23:32:42 -0500

    chore: added warning about wip features and security info

--- src/app/(dashboard)/classes/[classId]/assignments/[assignmentId]/client.tsx
+++ src/app/(dashboard)/classes/[classId]/assignments/[assignmentId]/client.tsx
@@ -7,8 +7,12 @@ import {
   IconFileUpload,
   IconHistory,
   IconLink,
+  IconLock,
+  IconLockOpen,
 } from "@tabler/icons-react";
 import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "@/app/_components/ui/button";
 import {
   Card,
@@ -76,33 +80,10 @@ export function Submission(): React.ReactElement {
         </TabsTrigger>
       </TabsList>
       <TabsContent className="min-h-[18.75rem]" value="text">
-        <TipTap
-          slotAfter={
-            <>
-              <Separator />
-              <div className="flex flex-row items-center justify-between p-4">
-                <div className="flex items-center gap-2 text-muted-foreground">
-                  <IconHistory className="h-5 w-5" /> Last saved 1 min ago
-                </div>
-                <Button>Submit</Button>
-              </div>
-            </>
-          }
-        />
+        <TextSubmission />
       </TabsContent>
       <TabsContent className="min-h-[18.75rem]" value="url">
-        <Card className="flex flex-col">
-          <CardHeader className="">
-            <CardTitle>Submit a URL</CardTitle>
-          </CardHeader>
-          <CardContent className="">
-            <Input placeholder="https://" />
-          </CardContent>
-          <Separator />
-          <CardFooter className="justify-end p-4 px-6">
-            <Button>Submit</Button>
-          </CardFooter>
-        </Card>
+        <UrlSubmission />
       </TabsContent>
       <TabsContent className="min-h-[18.75rem]" value="file">
         {/* <Card className="flex flex-col">
@@ -120,5 +101,89 @@ export function Submission(): React.ReactElement {
         <DropZone />
       </TabsContent>
     </Tabs>
+  );
+}
+
+function UrlSubmission(): React.ReactElement {
+  const [url, setUrl] = useState("");
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="">
+        <CardTitle>Submit a URL</CardTitle>
+      </CardHeader>
+      <CardContent className="">
+        <Input
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+          placeholder="https://"
+          value={url}
+        />
+      </CardContent>
+      <Separator />
+      <CardFooter className="justify-between p-4 px-6">
+        <div className="flex flex-row items-center gap-2 text-muted-foreground">
+          {url.startsWith("http://") && (
+            <>
+              <IconLockOpen className="h-5 w-5" /> This site doesn&lsquo;t use
+              HTTPS.{" "}
+              <Link
+                className="underline"
+                href="https://www.cloudflare.com/learning/ssl/why-is-http-not-secure/#:~:text=HTTPS%20is%20HTTP%20with%20encryption,far%20more%20secure%20than%20HTTP."
+              >
+                Learn More
+              </Link>
+            </>
+          )}{" "}
+          {url.startsWith("https://") && (
+            <>
+              <IconLock className="h-5 w-5" /> This site uses HTTPS.{" "}
+              <Link
+                className="underline"
+                href="https://www.cloudflare.com/learning/ssl/why-is-http-not-secure/#:~:text=HTTPS%20is%20HTTP%20with%20encryption,far%20more%20secure%20than%20HTTP."
+              >
+                Learn More
+              </Link>
+            </>
+          )}
+        </div>
+        <Button
+          onClick={() => {
+            toast.warning("You're early!", {
+              description: "This feature hasn't been implemented yet.",
+            });
+          }}
+        >
+          Submit
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+function TextSubmission(): React.ReactElement {
+  return (
+    <TipTap
+      slotAfter={
+        <>
+          <Separator />
+          <div className="flex flex-row items-center justify-between p-4">
+            <div className="flex items-center gap-2 text-muted-foreground">
+              <IconHistory className="h-5 w-5" /> Last saved 1 min ago
+            </div>
+            <Button
+              onClick={() => {
+                toast.warning("You're early!", {
+                  description: "This feature hasn't been implemented yet.",
+                });
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        </>
+      }
+    />
   );
 }
