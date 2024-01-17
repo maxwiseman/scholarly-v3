a752dea2c0c1a6442a7bf9e5e47a342aa3ba533e
maxwiseman <13maxwellwise@gmail.com>
2024-01-17 11:43:56 -0500

    chore: puppeteer runs on the backend now

--- apps/frontend/src/server/api/routers/aspen/get-assignments.ts
+++ apps/frontend/src/server/api/routers/aspen/get-assignments.ts
@@ -1,11 +1,10 @@
-import puppeteer, { type Browser } from "puppeteer-core";
-import { v4 as uuid } from "uuid";
 import { and, eq, sql } from "drizzle-orm";
-import { capitalize, goToAcademics, login } from "./lib";
-import { env } from "@/env";
-import { db } from "@/server/db";
-import { aspenAssignments, classes } from "@/server/db/schema";
+import { v4 as uuid } from "uuid";
+import { type AspenAssignment } from "@scholarly/backend/routes/get-assignments";
 import { getServerAuthSession } from "@/server/auth";
+import { db } from "@/server/db";
+import { aspenAssignments, classes, users } from "@/server/db/schema";
+import { env } from "@/env";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- We have a good type inference
 export async function getAssignments(id: string) {
@@ -13,107 +12,18 @@ export async function getAssignments(id: string) {
   const aspenIdData = await db.query.classes.findFirst({
     where: and(eq(classes.id, id), eq(classes.userId, session?.user.id || "")),
   });
+  const aspenCredentials = await db.query.users.findFirst({
+    where: eq(users.id, session?.user.id || ""),
+  });
   const aspenId = aspenIdData?.aspenId || "";
-  const options = {
-    args: [],
-    executablePath:
-      // eslint-disable-next-line no-nested-ternary -- This isn't that confusing
-      process.platform === "win32"
-        ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-        : process.platform === "linux"
-          ? "/usr/bin/google-chrome"
-          : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-  };
-  let browser: Browser;
-  if (process.env.VERCEL) {
-    browser = await puppeteer.connect({
-      browserWSEndpoint: `wss://chrome.browserless.io?token=${env.BROWSERLESS_API_KEY}`,
-    });
-  } else {
-    browser = await puppeteer.launch(options);
-  }
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
 
-  await page.setViewport({ width: 1920, height: 1080 });
-
-  // Login
-  await login(page, async () => {
-    await browser.close();
-  });
-
-  // Go to the Academics tab
-  await goToAcademics(page);
-
-  // Go to class by ID
-  await page.evaluate(
-    `doParamSubmit(2100, document.forms['classListForm'], '${aspenId}');`,
-  );
-
-  // Go to Assignments page
-  await page.waitForSelector("a[title='List of assignments']", {
-    timeout: 10000,
-  });
-  await page.click("a[title='List of assignments']");
-  await page.waitForSelector("#dataGrid", { timeout: 10000 });
-  if (!process.env.VERCEL_ENV) await page.screenshot({ path: "output.png" });
-
-  // Extract data
-  const name = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(2) a",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const category = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(3)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const pointsPossible = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(4)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const dateAssigned = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(5)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const dateDue = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(6)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const extraCredit = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(7)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const score = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(8) tr",
-    (elements) =>
-      elements.map(
-        (element) =>
-          element.querySelector("td:not(:has(.percentFieldContainer))")
-            ?.textContent,
-      ),
-  );
-  const feedback = await page.$$eval(
-    "#dataGrid tr:not(:first-of-type) > td:nth-of-type(9)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-
-  const data = name.map((assignmentName, index) => {
-    return {
-      name: name[index] || "",
-      category: category[index] || "",
-      pointsPossible: parseFloat(pointsPossible[index] ?? ""),
-      dateAssigned: new Date(Date.parse(dateAssigned[index] ?? "")),
-      dateDue: new Date(Date.parse(dateDue[index] ?? "")),
-      extraCredit: extraCredit[index] === "Y",
-      points: score[index]?.match(/^0\.0.*/)
-        ? 0
-        : parseFloat(score[index] ?? "") ||
-          capitalize(score[index]?.toString() ?? ""),
-      feedback: feedback[index] || "",
-    };
-  });
-
-  await browser.close();
+  const data = await fetch(`${env.BACKEND_URL}/get-assignments?id=${aspenId}`, {
+    headers: {
+      Authorization: `Basic ${btoa(
+        `${aspenCredentials?.aspenUsername}:${aspenCredentials?.aspenPassword}`,
+      )}`,
+    },
+  }).then((res) => res.json() as Promise<AspenAssignment[]>);
 
   await db
     .insert(aspenAssignments)
@@ -124,8 +34,8 @@ export async function getAssignments(id: string) {
           id: uuid(),
           userId: session?.user.id || "",
           classId: id,
-          dateAssigned: assignment.dateAssigned,
-          dateDue: assignment.dateDue,
+          dateAssigned: new Date(Date.parse(assignment.dateAssigned)),
+          dateDue: new Date(Date.parse(assignment.dateDue)),
         };
       }),
     ) // Replace with your schema and new data
--- apps/frontend/src/server/api/routers/aspen/get-categories.ts
+++ apps/frontend/src/server/api/routers/aspen/get-categories.ts
@@ -1,10 +1,9 @@
-import puppeteer, { type Browser } from "puppeteer-core";
+import { type AspenCategory } from "@scholarly/backend/routes/get-categories";
 import { and, eq } from "drizzle-orm";
-import { goToAcademics, login } from "./lib";
-import { env } from "@/env";
 import { getServerAuthSession } from "@/server/auth";
 import { db } from "@/server/db";
-import { classes } from "@/server/db/schema";
+import { classes, users } from "@/server/db/schema";
+import { env } from "@/env";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- We have good type inference
 export async function getCategories(id: string) {
@@ -12,75 +11,17 @@ export async function getCategories(id: string) {
   const aspenIdData = await db.query.classes.findFirst({
     where: and(eq(classes.id, id), eq(classes.userId, session?.user.id || "")),
   });
-  const aspenId = aspenIdData?.aspenId;
-  const options = {
-    args: [],
-    executablePath:
-      // eslint-disable-next-line no-nested-ternary -- This isn't that confusing
-      process.platform === "win32"
-        ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-        : process.platform === "linux"
-          ? "/usr/bin/google-chrome"
-          : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-  };
-  let browser: Browser;
-  if (process.env.VERCEL) {
-    browser = await puppeteer.connect({
-      browserWSEndpoint: `wss://chrome.browserless.io?token=${env.BROWSERLESS_API_KEY}`,
-    });
-  } else {
-    browser = await puppeteer.launch(options);
-  }
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
-
-  await page.setViewport({ width: 1920, height: 1080 });
-
-  // Login
-  await login(page, async () => {
-    await browser.close();
+  const aspenCredentials = await db.query.users.findFirst({
+    where: eq(users.id, session?.user.id || ""),
   });
-
-  // Go to the Academics tab
-  await goToAcademics(page);
-
-  // Go to class by ID
-  await page.evaluate(
-    `doParamSubmit(2100, document.forms['classListForm'], '${aspenId}');`,
-  );
-  await page.waitForSelector("#dataGridRight", { timeout: 10000 });
-  if (!process.env.VERCEL_ENV) await page.screenshot({ path: "output.png" });
-
-  // Extract data
-  const names = await page.$$eval(
-    "#dataGridRight tr:nth-of-type(even):not(:last-of-type):not(:nth-last-of-type(2)) > td:nth-of-type(1)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const weights = await page.$$eval(
-    "#dataGridRight tr:nth-of-type(even):not(:last-of-type):not(:nth-last-of-type(2)) > td:nth-of-type(3)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const values = await page.$$eval(
-    "#dataGridRight tr:nth-of-type(odd):not(first-of-type):not(:last-of-type):not(:nth-last-of-type(2)) > td:nth-of-type(2)",
-    (elements) => elements.map((element) => element.innerText),
-  );
-  const average = await page.$eval(
-    "#dataGridRight tr:nth-last-of-type(2) > td:nth-of-type(2)",
-    (element) => element.innerText,
-  );
-
-  const data = {
-    average: parseFloat(average),
-    categories: names.map((name, index) => {
-      return {
-        name: names[index],
-        weight: parseFloat(weights[index] ?? "") / 100,
-        value: parseFloat(values[index] ?? ""),
-      };
-    }),
-  };
-
-  await browser.close();
+  const aspenId = aspenIdData?.aspenId;
+  const data = await fetch(`${env.BACKEND_URL}/get-categories?id=${aspenId}`, {
+    headers: {
+      Authorization: `Basic ${btoa(
+        `${aspenCredentials?.aspenUsername}:${aspenCredentials?.aspenPassword}`,
+      )}`,
+    },
+  }).then((res) => res.json() as Promise<AspenCategory[]>);
 
   return data;
 }
