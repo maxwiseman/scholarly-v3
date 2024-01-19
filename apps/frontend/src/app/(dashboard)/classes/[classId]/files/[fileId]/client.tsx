56ddafe2f35901d79086f5e109ccb11e68ee5610
maxwiseman <13maxwellwise@gmail.com>
2024-01-19 16:22:27 -0500

    chore: added border-radius to file preview

--- apps/frontend/src/app/(dashboard)/classes/[classId]/files/[fileId]/client.tsx
+++ apps/frontend/src/app/(dashboard)/classes/[classId]/files/[fileId]/client.tsx
@@ -2,5 +2,9 @@
 import GoogleDocsViewer from "react-google-docs-viewer";
 
 export function FilePreview({ url }: { url: string }): React.ReactElement {
-  return <GoogleDocsViewer fileUrl={url} height="780px" width="100%" />;
+  return (
+    <div className="overflow-hidden rounded-md">
+      <GoogleDocsViewer fileUrl={url} height="780px" width="100%" />
+    </div>
+  );
 }
