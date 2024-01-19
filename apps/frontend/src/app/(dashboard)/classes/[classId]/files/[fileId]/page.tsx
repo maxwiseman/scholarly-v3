d2a5255807b3ccfa90cee334a80bff76dbd550ee
maxwiseman <13maxwellwise@gmail.com>
2024-01-19 16:30:38 -0500

    feat: format file size nicely

--- apps/frontend/src/app/(dashboard)/classes/[classId]/files/[fileId]/page.tsx
+++ apps/frontend/src/app/(dashboard)/classes/[classId]/files/[fileId]/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-col gap-2">
             <h1 className="mt-0 text-3xl font-bold">{fileData.display_name}</h1>
-            <div>Size: {fileData.size}</div>
+            <div>Size: {formatBytes(fileData.size)}</div>
           </div>
         </div>
         <Separator className="my-6" />
@@ -24,4 +24,27 @@ export default async function Page({
       </div>
     </div>
   );
+}
+
+// Courtesy of Stack Overflow Community Wiki: https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+function formatBytes(bytes: string | number, decimals = 2): string {
+  if (!Number(bytes)) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = [
+    "Bytes",
+    "KiB",
+    "MiB",
+    "GiB",
+    "TiB",
+    "PiB",
+    "EiB",
+    "ZiB",
+    "YiB",
+  ];
+
+  const i = Math.floor(Math.log(Number(bytes)) / Math.log(k));
+
+  return `${parseFloat((Number(bytes) / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 }
