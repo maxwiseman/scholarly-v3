214b7e7d13597c5d0b734a337ac7ac5e582766f6
maxwiseman <13maxwellwise@gmail.com>
2024-01-23 15:07:35 -0500

    feat: added ui for when no classes are found

--- apps/frontend/src/app/(dashboard)/dashboard/page.tsx
+++ apps/frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button, LinkButton } from "@/app/_components/ui/button";
 import { ClassCard } from "./class-card";
 import { Separator } from "@/app/_components/ui/separator";
 import { queryOpts } from "@/lib/utils";
@@ -7,6 +8,25 @@ import { api } from "@/trpc/react";
 
 export default function Page(): React.ReactElement {
   const classFetcher = api.user.getClasses.useQuery(undefined, queryOpts);
+
+  if (classFetcher.isFetched && classFetcher.data?.length === 0) {
+    return (
+      <div className="p-8 py-10">
+        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+          Dashboard
+        </h1>
+        <Separator className="my-4" />
+        <div className="flex h-full w-full flex-col items-center justify-center py-8">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h2 className="text-xl text-muted-foreground">No classes found!</h2>
+            <LinkButton variant={"secondary"} href="/onboarding">
+              Add your classes
+            </LinkButton>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="p-8 py-10">
