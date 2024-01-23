c59d71ae17384caca6681b7b2ed8140e0b1c93ef
maxwiseman <13maxwellwise@gmail.com>
2024-01-23 15:27:28 -0500

    feat: added not-found and error pages

---
+++ apps/frontend/src/app/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button, LinkButton } from "./_components/ui/button";
+
+export default function Page() {
+  const router = useRouter();
+
+  return (
+    <main>
+      <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
+        Woah there! Something went wrong! If this keeps happening, please
+        contact us.
+        <div className="flex flex-row gap-2">
+          <Button
+            onClick={() => {
+              router.refresh();
+            }}
+          >
+            Reload the page
+          </Button>
+          <LinkButton variant={"secondary"} href="mailto:max@maxwiseman.io">
+            Contact us
+          </LinkButton>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ apps/frontend/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { Button, LinkButton } from "./_components/ui/button";
+
+export default function Page() {
+  return (
+    <main>
+      <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
+        This page was not found
+        <div>
+          <LinkButton href="/">Go Home</LinkButton>
+        </div>
+      </div>
+    </main>
+  );
+}
