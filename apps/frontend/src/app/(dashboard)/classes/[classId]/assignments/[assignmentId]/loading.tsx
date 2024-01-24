6edcc9143c757c9bae63b0f333fb61b25a252fb2
maxwiseman <13maxwellwise@gmail.com>
2024-01-24 18:00:00 -0500

    fix: fixed assignment page loading state

--- apps/frontend/src/app/(dashboard)/classes/[classId]/assignments/[assignmentId]/loading.tsx
+++ apps/frontend/src/app/(dashboard)/classes/[classId]/assignments/[assignmentId]/loading.tsx
@@ -1,23 +1,44 @@
 import React from "react";
 import { Separator } from "@/app/_components/ui/separator";
 import { Skeleton } from "@/app/_components/ui/skeleton";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/app/_components/ui/resizable";
 
 export default function Page(): React.ReactElement {
   return (
-    <div className="flex w-full justify-center">
-      <div className="w-full max-w-4xl">
-        <div className="flex flex-col gap-2">
-          <Skeleton className="h-9 w-96" />
-          <div>
-            Points Possible:{" "}
-            <Skeleton className="inline-block h-[1.125rem] w-4" />
-          </div>
-          <div>
-            Date Due: <Skeleton className="inline-block h-[1.125rem] w-28" />
+    <ResizablePanelGroup
+      className="h-full max-h-min w-full max-w-full overflow-visible"
+      direction="horizontal"
+      style={{ overflow: "shown" }}
+    >
+      <ResizablePanel className="flex h-max justify-center" defaultSize={100}>
+        <div className="w-full max-w-4xl">
+          <div className="flex flex-col items-center justify-between">
+            <div className="flex w-full flex-col gap-2">
+              <Skeleton className="h-9 w-96" />
+              <div>
+                Points Possible:{" "}
+                <Skeleton className="inline-block h-[1.125rem] w-4" />
+              </div>
+              <div>
+                Date Due:{" "}
+                <Skeleton className="inline-block h-[1.125rem] w-28" />
+              </div>
+            </div>
+            <Separator className="my-6" />
           </div>
         </div>
-        <Separator className="my-6" />
+      </ResizablePanel>
+      <div className="relative h-full">
+        <ResizableHandle
+          className="sticky top-[3.5rem] mx-8 h-[calc(100vh-3.5rem)] rounded-full"
+          withHandle
+        />
       </div>
-    </div>
+      <ResizablePanel className="flex h-max justify-center" defaultSize={0} />
+    </ResizablePanelGroup>
   );
 }
