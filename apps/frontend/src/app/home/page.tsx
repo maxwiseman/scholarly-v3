2955c3fdf1615128a3714d9cf251fbd778f0935c
maxwiseman <13maxwellwise@gmail.com>
2024-01-26 14:58:39 -0500

    chore: updated tagline

--- apps/frontend/src/app/home/page.tsx
+++ apps/frontend/src/app/home/page.tsx
@@ -10,7 +10,7 @@ export default function Page(): React.ReactElement {
       <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
         <Badge variant="secondary">Beta</Badge>
         <h1 className="text-8xl font-bold">Scholarly</h1>
-        <h2 className="text-3xl">Welcome to the future of learning</h2>
+        <h2 className="text-3xl">All your data in one place</h2>
         <LinkButton className="mt-6" href="/sign-in">
           Get started <IconArrowRight className="ml-2 h-4 w-4" />
         </LinkButton>
