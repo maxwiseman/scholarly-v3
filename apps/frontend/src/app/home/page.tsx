c946cc82aa638fca74a323912feaeade20c94809
maxwiseman <13maxwellwise@gmail.com>
2024-01-28 17:54:27 -0500

    fix: fixed light mode landing page

--- apps/frontend/src/app/home/page.tsx
+++ apps/frontend/src/app/home/page.tsx
@@ -8,10 +8,10 @@ import { Spotlight } from "../_components/ui/spotlight";
 export default function Page(): React.ReactElement {
   return (
     <main>
-      <div className="bg-grid-white/[0.02] flex h-screen w-screen flex-col items-center justify-center gap-2">
-        <Spotlight />
+      <div className="dark:bg-grid-white/[0.02] bg-grid-black/[0.04] flex h-screen w-screen flex-col items-center justify-center gap-2">
+        <Spotlight className="invert" />
         <Badge variant="secondary">Beta</Badge>
-        <h1 className="h-[5rem] min-h-max bg-gradient-to-b from-foreground to-neutral-600 bg-clip-text text-center text-7xl font-bold text-transparent md:h-[6.4rem]  md:text-8xl">
+        <h1 className="h-[5rem] min-h-max bg-gradient-to-b from-foreground to-neutral-400 bg-clip-text text-center text-7xl font-bold text-transparent dark:to-neutral-600 md:h-[6.4rem]  md:text-8xl">
           Scholarly
         </h1>
         <h2 className="text-lg text-neutral-500">Learning made easy</h2>
