1da4492c939c4e2ce964fa907c900813c8b55c28
maxwiseman <13maxwellwise@gmail.com>
2024-02-05 13:35:48 -0500

    feat: added more view modes to study sets

--- apps/frontend/src/app/(dashboard)/study/page.tsx
+++ apps/frontend/src/app/(dashboard)/study/page.tsx
@@ -8,7 +8,7 @@ export default function Page(): React.ReactElement {
       <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
         Study Sets
       </h1>
-      <Separator className="my-4" />
+      <Separator className="mt-4" />
       <div className="grid grid-cols-1 gap-4 py-8 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3">
         {sets.map((set) => {
           return <MenuCard cardSet={set} key={set.slug} />;
---
+++ apps/frontend/src/app/(dashboard)/study/sets/[...setInfo]/client.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { IconDotsVertical } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/app/_components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+
+export function Options({
+  setInfo,
+}: {
+  setInfo: string[];
+}): React.ReactElement {
+  const router = useRouter();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="outline">
+          <IconDotsVertical className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>View as...</DropdownMenuSubTrigger>
+          <DropdownMenuSubContent>
+            <DropdownMenuCheckboxItem
+              checked={Boolean(setInfo[1] !== "stack" && setInfo[1] !== "quiz")}
+              onClick={() => {
+                router.push(`/study/sets/${setInfo[0]}/grid`);
+              }}
+            >
+              Grid
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+              checked={Boolean(setInfo[1] === "stack")}
+              onClick={() => {
+                router.push(`/study/sets/${setInfo[0]}/stack`);
+              }}
+            >
+              Stack
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+              checked={Boolean(setInfo[1] === "quiz")}
+              onClick={() => {
+                router.push(`/study/sets/${setInfo[0]}/quiz`);
+              }}
+            >
+              Quiz
+            </DropdownMenuCheckboxItem>
+          </DropdownMenuSubContent>
+        </DropdownMenuSub>
+        <DropdownMenuItem disabled>Edit set</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
