4d27f336b212cc70455afe9eaa975787116104f6
maxwiseman <13maxwellwise@gmail.com>
2024-02-05 13:53:55 -0500

    chore: setup vercel web analytics

--- apps/frontend/src/app/_components/next-ui-provider.tsx
+++ apps/frontend/src/app/_components/next-ui-provider.tsx
@@ -2,12 +2,14 @@
 
 import { NextUIProvider, type NextUIProviderProps } from "@nextui-org/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export function NextUIClientProvider(
   props: NextUIProviderProps,
 ): React.ReactElement {
   return (
     <>
+      <Analytics />
       <SpeedInsights />
       <NextUIProvider {...props} />
     </>
